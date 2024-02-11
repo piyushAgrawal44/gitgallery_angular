1bdd3f537f03920e02e7c85e6a555611890c067a
Piyush <piyushbansal941@gmail.com>
2024-02-11 13:36:07 +0530

    added few more comment and version 1.0 completed with github api auth setup on both local and server  env variable on netlify

--- generate-env.js
+++ generate-env.js
@@ -1,17 +1,18 @@
 const fs = require('fs');
 
-// Read the template file
+// Read the template env file
 fs.readFile('src/environments/environment.sample.ts', 'utf8', (err, data) => {
+
   if (err) {
     console.error('Error reading template file:', err);
     return;
   }
 
   // Replace placeholders with actual environment variable values
-  const environment = data.replace(/##API_URL##/g, process.env.GITHUB_TOKEN || 'default-token');
+  const environment = data.replace(/##GITHUB_TOKEN##/g, process.env.GITHUB_TOKEN || 'default-token');
   // Add more replacements as needed for other environment variables
 
-  // Write the environment file
+  // Write the environment file on the go .prod file
   fs.writeFile('src/environments/environment.prod.ts', environment, 'utf8', err => {
     if (err) {
       console.error('Error writing environment file:', err);
@@ -19,6 +20,8 @@ fs.readFile('src/environments/environment.sample.ts', 'utf8', (err, data) => {
     }
     console.log('Environment file generated successfully!');
   });
+
+  // Write the environment file on the go .ts file
   fs.writeFile('src/environments/environment.ts', environment, 'utf8', err => {
     if (err) {
       console.error('Error writing environment file:', err);
